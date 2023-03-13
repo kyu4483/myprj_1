d76a7c90a66cb4619ee0f59add46e6977bb768df
MoyaEgun <kyu4483@gmail.com>
2023-03-13 15:56:34 +0900

    수정기능 추가

--- test.js
+++ test.js
@@ -2,7 +2,11 @@ const $btnAdd = document.getElementById('btnAdd');
 $btnAdd.addEventListener('click', () => {
   console.log('클릭');
 });
-const $btnDel = document.getElementById('btnAdd');
+const $btnDel = document.getElementById('btnDel');
 $btnDel.addEventListener('click', () => {
   console.log('삭제');
+});
+const $btnFix = document.getElementById('btnFix');
+$btnFix.addEventListener('click', () => {
+  console.log('수정');
 });
